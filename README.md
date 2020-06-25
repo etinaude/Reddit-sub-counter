36f11e0873a8cbf0610b5be6c43d8dd32c97dbf9
Etienne <etinaude@gmail.com>
2020-06-26 00:05:49 +1200

    set up file system

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Reddit-sub-counter
+
+this will display the number of membres of the [UnleashSpace subreddit](www.reddit.com/r/UnleashSpace) on a small display
+using an ardiuno
---
+++ ardiuno_code/ardiuno_code.ino
@@ -0,0 +1,17 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("hello, world!");
+}
+
+void loop() {
+
+}
